a83f02117d6afd81583ce086edccd9010b1ae63f
Sophie Kish <skish2@illinois.edu>
2021-02-07 01:16:12 -0600

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App_Ed_Res from './educating-resources/App_Ed_Res_Function'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <App_Ed_Res /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
