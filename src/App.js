1ffca68c56f91a0b07a86534fdf8352196852e7b
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2021-02-07 15:07:20 -0800

    linked vr.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import App_virtual_page from "./virtual-page/App_virtual_page";
 import ImageGallery from "./vr";
 function App() {
   return (
     <div className="App">
-      <a href = "vr">VR</a>
       <Router>
-        <Route path="vr" exact component= {ImageGallery} />
+        <Switch>
+          <Route exact path="./vr"> </Route>  
+            <Link to="./vr"><ImageGallery /></Link>
+        </Switch>
+        
       </Router>
     </div>
   );
