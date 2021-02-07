9a98fdcd3037dc71e38ce53b3e16d0f8cb7e02ab
George Moaites <geodude@Georges-MBP.fios-router.home>
2021-02-06 23:14:43 -0500

    Initial commit to add images

---
+++ educating-resources/ResourcesPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const resource = () => {
+    return (
+        <div> 
+            <h1>
+            How You Can Create Change 
+            </h1>
+            <h2>
+            Personal Change
+            </h2>
+            <p>
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+            </p>
+            <h2>
+            Outreach
+            </h2>
+            <p>
+
+            </p>
+       </div>
+    )
+};
+
+export default resource; 
