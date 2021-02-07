4c83bf6d5e10bae98a6f20a262e789da29cb8cf3
sank27 <47614581+sank27@users.noreply.github.com>
2021-02-07 11:42:22 -0800

    Added virtual page router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import App_virtual_page from './virtual-page/App_virtual_page'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         
-        <p>Testing</p>
+        
+        <Router>
+          <Link to="/App_virtual_page">Virtual page</Link>
+          <Route path="/App_virtual_page" component={App_virtual_page}/>
+        </Router>
 
-        < App_virtual_page />
+        
         
       </header>
     </div>
