5004489b758952e3c9eef1ade6aeba6caf342bb0
Daniel Santana <santana.danny4g@gmail.com>
2021-02-07 17:15:30 -0800

    Form style

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ function App() {
 
       <form action="">
         <div id='flex'>
-        <label for="ename"> What is your average electricity per month? </label>
+        <label htmlFor="ename"> What is your average electricity per month? </label>
         <input type="text" id="ename"></input>
         
-        <label for="ename"> What is your average natural gas bill per month?</label>
+        <label htmlFor="ename"> What is your average natural gas bill per month?</label>
         <input type="text" id="ename"></input>
-        <label for="ename">To determine your waste emission please enter how man people live in your household: </label>
+        <label htmlFor="ename">To determine your waste emission please enter how man people live in your household: </label>
         <input type="text" id="ename"></input>
+        <button>Submit</button>
         </div>
       </form>
 
-      <PieChart  data = {[
-             { title: 'One', value: 10, color: '#E38627' },
+      <PieChart paddingAngle= {1} style= {{height: '450px', width: '450px'}} 
+      data = {[
+             { title: 'One', value: 25, color: '#E38627'},
              { title: 'Two', value: 15, color: '#C13C37' },
-             { title: 'Three', value: 20, color: '#6A2135' },
-        ]} />
-
+             { title: 'Three', value: 30, color: '#6A2135' },
+        ]}
+        label= {({ dataEntry }) => dataEntry.value}
+        labelStyle={(index) => ({
+          fontSize: '10px',
+          fontFamily: 'sans-serif',
+        })}
+        />
+        
     </div>
   );
 }
